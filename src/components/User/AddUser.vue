a5235932c8db04dfd2414e24627993e77d95548d
C B Sajan <sajan@ckhathri.com>
2022-05-18 21:12:15 +0530

    03 Posting

--- src/components/User/AddUser.vue
+++ src/components/User/AddUser.vue
@@ -1,23 +1,30 @@
 <template>
-    <h2>Add user</h2>
+    <h2>Add users</h2>
 
-    <div class="form-group">
-        <label>Name</label>
-        <input class="form-control" type="text" v-model="user.name">
+    <div v-show="loading">
+        <app-loader></app-loader>
     </div>
+    <div v-show="!loading">
+        <div class="form-group">
+            <label>Name</label>
+            <input class="form-control" type="text" v-model="user.name">
+        </div>
 
-    <div class="form-group">
-        <label>Lastname</label>
-        <input class="form-control" type="text" v-model="user.lastname">
+        <div class="form-group">
+            <label>Lastname</label>
+            <input class="form-control" type="text" v-model="user.lastname">
+        </div>
+
+        <button class="btn btn-primary" @click="submit">Submit</button>
     </div>
-
-    <button class="btn btn-primary" @click="submit">Submit</button>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
+            loading: false,
             user: {
                 name: '',
                 lastname: ''
@@ -26,7 +33,27 @@ export default {
     },
     methods: {
         submit() {
-            console.log(this.user)
+            this.loading = true;
+            axios({
+                method: 'post',
+                url: 'http://localhost:3004/users',
+                data: this.user
+            })
+                .then(() => {
+                    this.$toast.success('The user has been added', {
+                        position: 'bottom',
+                        duration: false
+                    });
+                })
+                .catch(error => {
+                    this.$toast.error(error, {
+                        position: 'bottom',
+                        duration: false
+                    });
+                })
+                .finally(() => {
+                    this.loading = false;
+                })
         }
     }
 }
