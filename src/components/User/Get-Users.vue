4700483d3c0ec87e4846fc8e32fffb6ac202f566
C B Sajan <sajan@ckhathri.com>
2022-05-18 20:40:11 +0530

    Getting Users using json-server

--- src/components/User/Get-Users.vue
+++ src/components/User/Get-Users.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="row">
 
-        <div class="col-auto mb-4">
+        <div class="col-auto mb-4" v-for="(user, index) in users" :key="index">
             <div class="card" style="width: 14rem;">
-                <img class="card-img-top" src="https://placebeard.it/300/300" alt="Card image cap">
+                <img class="card-img-top" :src="'https://placebeard.it/300/300?' + user.id" alt="Card image cap">
                 <div class="card-body">
                     <div class="card-text">
-                        <strong>Name:</strong> Francis
+                        <strong>Name:</strong> {{ user.name }}
                     </div>
                     <div class="card-text">
-                        <strong>Lastname:</strong> Jones
+                        <strong>Lastname:</strong> {{ user.lastname }}
                     </div>
                 </div>
             </div>
@@ -21,4 +21,28 @@
 
 
 <script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    methods: {
+        loadUsers() {
+            axios.get('http://localhost:3004/users').then(response => {
+
+                this.users = response.data
+                console.log(this.users)
+            }).catch(error => {
+                console.log(error)
+            })
+
+        }
+    },
+    mounted() {
+        console.log("Loading Data")
+        this.loadUsers()
+    }
+}
 </script>
